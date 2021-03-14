c930d2beb3a6b7b5765c88e449c1de15431611a9
Matt Haynie <mrhaynie@live.com>
2021-03-13 18:33:31 -0800

    issues with exception rethrowing, apparently

--- test/coroutine_task_test.cpp
+++ test/coroutine_task_test.cpp
@@ -12,6 +12,31 @@
 
 using namespace std::chrono_literals;
 
+struct test_noncopyable_struct
+{
+	static constexpr char TEST_STR[] = "A string that does not fit into the buffer for SSO.";
+
+	test_noncopyable_struct()
+	{
+		value = TEST_STR;
+	}
+	~test_noncopyable_struct()
+	{
+		assert(!strcmp(value.c_str(), TEST_STR));
+	}
+
+	std::string value;
+};
+
+#ifdef _WIN32
+#define PRINT_EX_MSG() \
+	char buf[256]; \
+	sprintf_s(buf, "%s(%i | %p)\n", __FUNCSIG__, m_InstanceIndex, this); \
+	OutputDebugStringA(buf);
+#else
+#define PRINT_EX_MSG() // Not implemented
+#endif
+
 struct dummy_exception : std::nested_exception
 {
 	inline static std::atomic<int32_t> s_InstanceCount;
@@ -19,47 +44,40 @@ struct dummy_exception : std::nested_exception
 	dummy_exception(int v) :
 		m_Value(v)
 	{
-#ifdef _WIN32
-		char buf[256];
-		sprintf_s(buf, "%s(%i)\n", __FUNCSIG__, m_InstanceIndex);
-		OutputDebugStringA(buf);
-#endif
+		PRINT_EX_MSG();
 	}
+
+	~dummy_exception()
+	{
+		PRINT_EX_MSG();
+	}
+
 #if 0
 	dummy_exception(dummy_exception&& other) :
 		std::nested_exception(std::move(other)),
 		m_Value(other.m_Value)
 	{
-#ifdef _WIN32
-		char buf[256];
-		sprintf_s(buf, "%s(%i = %i)\n", __FUNCSIG__, m_InstanceIndex, other.m_InstanceIndex);
-		OutputDebugStringA(buf);
-#endif
+		PRINT_EX_MSG();
 	}
 	dummy_exception(const dummy_exception& other) :
 		std::nested_exception(other),
 		m_Value(other.m_Value)
 	{
-#ifdef _WIN32
-		char buf[256];
-		sprintf_s(buf, "%s(%i = %i)\n", __FUNCSIG__, m_InstanceIndex, other.m_InstanceIndex);
-		OutputDebugStringA(buf);
-#endif
+		PRINT_EX_MSG();
 	}
 #else
-	dummy_exception(dummy_exception&&) = delete;
-	dummy_exception(const dummy_exception&) = delete;
-#endif
-	~dummy_exception()
+	dummy_exception(const dummy_exception& other) :
+		std::nested_exception(other),
+		m_Value(std::move(other.m_Value)),
+		m_TestStruct(std::move(other.m_TestStruct))
 	{
-#ifdef _WIN32
-		char buf[256];
-		sprintf_s(buf, "%s(%i)\n", __FUNCSIG__, m_InstanceIndex);
-		OutputDebugStringA(buf);
-#endif
 	}
+	//dummy_exception(dummy_exception&&) = delete;
+	//dummy_exception(const dummy_exception&) = delete;
+#endif
 
 	int m_Value;
+	test_noncopyable_struct m_TestStruct;
 	int m_InstanceIndex = s_InstanceCount++;
 };
 
@@ -86,8 +104,8 @@ TEST_CASE("task - standard threaded std::nested_expection sanity check")
 		//{
 			try
 			{
-				std::rethrow_exception(dummy_exception_ptr);
-				//throw dummy_exception(__LINE__);
+				//std::rethrow_exception(dummy_exception_ptr);
+				throw dummy_exception(__LINE__);
 			}
 			catch (const dummy_exception& e)
 			{
@@ -119,15 +137,20 @@ TEST_CASE("task - exceptions from other threads")
 	{
 		try
 		{
-			co_await tp.co_add_task();
+			//co_await tp.co_add_task();
 			co_await producerTask;
 		}
 		catch (const dummy_exception& e)
 		{
 			eValue = e.m_Value;
-			throw dummy_exception(__LINE__);
+			//throw dummy_exception(__LINE__);
 		}
+
+		__debugbreak();
 	}(producerTask, eValue, tp);
+
+	const auto& address = consumerTask.m_Handle.address();
+	const auto& promise = consumerTask.m_Handle.promise();
 
 	// Just some random waits that should be valid
 	consumerTask.wait();
